0a22e9b7bfd35c27c8f356ea5e6255ba8a46a3fe
m <m@m-macos.local>
2017-03-02 14:29:30 -0800

    changed grammar to madmaan.ohm, reading in madmaan.ohm in madmaan.js

--- madmaan.js
+++ madmaan.js
@@ -1,34 +1,8 @@
 const ohm = require('ohm-js');
+const fs = require('fs');
 
 /* eslint-disable no-unused-vars, no-useless-escape */
-const madmaanGrammar = ohm.grammar(`madmaan {
-  Program   = Body
-  Body      = (Stmt "!")+
-  Stmt      = VarDec | IfStmt | WhileStmt | ExpStmt | ForStmt
-  ExpStmt   = Exp
-  ForStmt   = "for" VarDec "!" Exp "!" Exp "!" Body "!"
-  IfStmt    = "if" Exp "then" Body ("elif" Exp "then" Body)* ("else" Body)* "!"
-  WhileStmt = "while" Exp "do" Body "!"
-  VarDec    = id "is" Exp
-  Exp       = BinExp | FuncExp | UnExp | CallExp | Literal | id
-  FuncExp   = "(" (id ("," id)*)* ")" "=>" Body "!"
-  BinExp    = Exp binop Exp  -- binop
-            | Exp relop Exp  -- relop
-            | Exp mulop Exp  -- mulop
-            | Exp addop Exp  -- addop
-  UnExp     = unaop Exp  -- neg
-            = id unaop   -- inc
-  CallExp   = Exp
-  Literal   = boollit | intlit | stringlit
-  keywords  = ("if" | "else" | "elif" | "or" | "is"
-               | "and" | "false" | "true" | "while" | "do")
-  id        = ~keywords alnum+
-  binop     = "or" | "and"
-  mulop     = "*" | "/" | "%"
-  addop     = "+" | "-"
-  relop     = "==" | ">" | "<" | "<=" | ">=" | "~="
-  unaop     = "-" | "~" | "++" | "--"
-  boollit   = "false" | "true"
-  intlit    = digit+
-  stringlit = "\"" (~"\"" any)* "\""
-}`);
+const madmaanGrammar = ohm.grammar(fs.readFile('/etc/passwd', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+}));
