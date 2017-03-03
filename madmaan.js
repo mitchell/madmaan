3bcd7548675e25e0f10dcea3b020897f5b404908
mitchelljfs <mitchelljfsimon@gmail.com>
2017-03-03 15:32:34 -0800

    added precedence to expressions

--- madmaan.js
+++ madmaan.js
@@ -2,7 +2,7 @@ const ohm = require('ohm-js');
 const fs = require('fs');
 
 /* eslint-disable no-unused-vars, no-useless-escape */
-const madmaanGrammar = ohm.grammar(fs.readFile('/etc/passwd', (err, data) => {
+const madmaanGrammar = ohm.grammar(fs.readFile('./madmaan.ohm', (err, data) => {
   if (err) throw err;
-  console.log(data);
+  console.log(data); // eslint-disable-line no-console
 }));
