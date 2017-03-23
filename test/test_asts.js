a52a49738a6c993f2587fcd998bbbcbd3f895b2e
mitchelljfs <mitchelljfsimon@gmail.com>
2017-03-23 15:10:34 -0700

    create gh-pages branch for gh pages website

--- test/test_asts.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const mad = require('../madmaan.js');
-
-/* eslint-disable no-undef */
-describe('madmaan ast', () => {
-  it('should match test program \'x4 = 4!\'', () => {
-    assert.equal(mad.parse('./test/testprog1.mad'), '(Program (Body (VarDec x4 (Literal 4))))');
-  });
-});
