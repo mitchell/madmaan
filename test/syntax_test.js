b3a11fed8c9a5ed0867f134c1defaf381f95c8ef
Nick Sunga <nick.sunga95@gmail.com>
2017-03-24 13:59:56 -0700

    More syntax tests. Formatting lowkey ugly

--- test/syntax_test.js
+++ test/syntax_test.js
@@ -17,4 +17,26 @@ describe('Madmaan Syntax', () => {
     const match = parse('9 + 10!');
     assert.ok(match.succeeded());
   });
+
+  it('"showMe() => "Hello World"!!" should be a valid function', () => {
+    const match = parse('showMe() => "Hello World"!!');
+    assert.ok(match.succeeded());
+  });
+
+  it('"showMe() => "Hello World"!!!" should not be a valid function', () => {
+    const match = parse('showMe() => "Hello World"!!!');
+    if (match.succeeded() == false) {
+        assert.ok(true);
+    }
+  });
+
+  it('"while true do 1+1!!!" be a valid while loop', () => {
+    const match = parse('while true do 1+1!!!');
+    assert.ok(match.succeeded());
+  });
+
+  it('"if x > y then showMe() => "x > y"!!!!" be a valid if statement', () => {
+    const match = parse('if x > y then showMe() => "x > y"!!!!');
+    assert.ok(match.succeeded());
+  });
 });
