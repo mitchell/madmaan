28b28e51956c41ec51a3dbbcb9c96c5116725b80
Nick Sunga <nick.sunga95@gmail.com>
2017-04-23 21:27:11 -0700

    added types and error. Lowkey dont know what im doinggggg

---
+++ entities/type.js
@@ -0,0 +1,65 @@
+const error = require('../error');
+
+/* eslint-disable no-unused-vars */
+
+class Type {
+  constructor(type) {
+    this.type = type;
+  }
+
+  mustBeInt(message, location) {
+    return this.mustBeCompatibleWith(Type.INT, message);
+  }
+
+  mustBeFloat(message, location) {
+    return this.mustBeCompatibleWith(Type.FLOAT, message);
+  }
+
+  mustBeBool(message, location) {
+    return this.mustBeCompatibleWith(Type.BOOL, message);
+  }
+
+  mustBeString(message, location) {
+    return this.mustBeCompatibleWith(Type.STRING, message);
+  }
+
+  mustBeChar(message, location) {
+    return this.mustBeCompatibleWith(Type.CHAR, message);
+  }
+
+  mustBeObject(message, location) {
+    return this.mustBeCompatibleWith(Type.OBJECT, message);
+  }
+
+  mustBeCompatibleWith(otherType, message, location) {
+    if (!this.isCompatibleWith(otherType)) {
+      return error(message, location);
+    }
+    return true;
+  }
+
+  mustBeMutuallyCompatibleWith(otherType, message, location) {
+    if (!(this.isCompatibleWith(otherType || otherType.isCompatibleWith(this)))) {
+      return error(message, location);
+    }
+    return true;
+  }
+
+  isCompatibleWith(otherType) {
+    return this === otherType || this === Type.ARBITRARY || otherType === Type.ARBITRARY;
+  }
+
+  toString() {
+    return (`(Type ${this.type})`);
+  }
+}
+
+Type.INT = new Type('int');
+Type.FLOAT = new Type('float');
+Type.BOOL = new Type('boolean');
+Type.STRING = new Type('string');
+Type.CHAR = new Type('char');
+Type.OBJECT = new Type('object');
+Type.NULL = new Type('null');
+
+module.exports = Type;
