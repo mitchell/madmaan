e90d79a7db5d292269c389f3e8d86da34516974a
mitchelljfs <mitchelljfsimon@gmail.com>
2017-04-25 20:08:05 -0700

    added a test for function declarations, got it to pass, bueno

--- entities/funcdec.js
+++ entities/funcdec.js
@@ -5,6 +5,7 @@ class FuncDec {
   constructor(id, params, body) {
     this.id = id;
     this.params = params;
+    console.log(this.params);
     this.body = body;
   }
 
@@ -17,27 +18,27 @@ class FuncDec {
 
     // Each parameter will be declared in the function's scope, mixed in
     // with the function's local variables. This is by design.
-    this.params.forEach(p => p.analyze(localContext));
+    this.params.params.forEach(p => p.analyze(localContext));
 
     // Make sure all required parameters come before optional ones, and
     // gather the names up into sets for quick lookup.
     this.requiredParameterNames = new Set();
     this.allParameterNames = new Set();
-    this.params.forEach((p) => {
+    this.params.params.forEach((p) => {
       this.allParameterNames.add(p.id);
       if (p.isRequired) {
         this.requiredParameterNames.add(p.id);
         if (this.requiredParameterNames.size < this.allParameterNames.size) {
           throw new Error('Required parameter cannot appear after an optional parameter');
         }
-    } // don't think I need this
+      } // don't think I need this
     });
 
     // The function itself will be added to the outer scope, but only AFTER
     // the parameters have been inserted into the inner scope. This is because
     // we don't want to allow the function itself to be called in the default
     // expression of any parameter. Think about it. :)
-    context.add(this);
+    context.addVariable(this);
 
     // Now we analyze the body with the local context. Note that recursion is
     // allowed, because we've already inserted the function itself into the
@@ -46,7 +47,7 @@ class FuncDec {
     // variable with the same name as the function inside the function, you'll
     // shadow it, which would probably be not a good idea.
     if (this.body) {
-      this.body.forEach(s => s.analyze(localContext));
+      this.body.statements.forEach(s => s.analyze(localContext));
     }
   }
 }
