d85a7b97347e3c0910a58578a27f03df4bd5b069
averdin2 <averdin2@lion.lmu.edu>
2017-04-27 16:39:53 -0700

    working on ifstmt entity

--- entities/ifstmt.js
+++ entities/ifstmt.js
@@ -1,10 +1,24 @@
 class IfStmt {
-  constructor(expStmt, body) {
-    this.expStmt = expStmt;
-    this.body = body;
+  constructor(ifCase, ifBody, elifCases, elifBodys, elseCase) {
+    this.ifCase = ifCase;
+    this.ifBody = ifBody;
+    this.elifCases = elifCases;
+    this.elifBodys = elifBodys;
+    this.elseCase = elseCase;
   }
   toString() {
+    const result = `(IfStmt ${this.ifCase} ${this.body})`;
+    this.elifCases.forEach(s => 'elif '.concat(s));
+    this.elifBodys.forEach(s => 'then '.concat(s));
+    console.log(this.elifCases);
     return `(IfStmt ${this.expStmt} ${this.body})`;
+  }
+  analyze(context) {
+    const expType = this.expStmt.analyze(context);
+    if (!expType.boolCheck()) {
+      throw new Error('Expected boolean condition');
+    }
+    this.body.analyze(context);
   }
 }
 
